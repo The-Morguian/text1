ac65797be9ece755c2fbe96e6e4761f5d14578a5
muzhou0000 <55180633+muzhou0000@users.noreply.github.com>
2020-02-02 02:05:21 +0800

    改上去

---
+++ TM1/Assets/Script/move.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    #region 數據
+    [Header("速度"), Range(50, 2000)]
+    public float speed;
+    [Header("旋轉"), Range(50, 200)]
+    public float turn;
+    public Rigidbody rig;
+    public Transform Tra;
+    public Animator ani;
+
+
+    #endregion
+
+    private void Update()
+    {
+        Walk();
+        Rota();
+        Attack();
+    }
+
+
+    #region 行動
+
+    void Walk()
+    {
+        if (ani.GetCurrentAnimatorStateInfo(0).IsName("Attack")) return;
+        float v = Input.GetAxis("Vertical");
+        rig.AddForce(Tra.forward * v * speed * Time.deltaTime);
+        ani.SetBool("walk", v != 0);
+    }
+    void Rota()
+    {
+        float h = Input.GetAxis("Horizontal");
+        Tra.Rotate(0, turn * h * Time.deltaTime, 0);
+
+    }
+    void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ani.SetTrigger("Attack");
+        }
+
+    }
+
+    #endregion
+
+}
